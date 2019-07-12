125b1309ebb3202cbaac2b76e35cadd53d0edbf9
Nke0628 <ooo_diva@yahoo.co.jp>
2019-07-13 01:07:16 +0900

    画像表示修正

--- public/js/post.js
+++ public/js/post.js
@@ -33,7 +33,7 @@ function ajustWidth(){
 		$('.test').addClass('mt-3');
 		$('.test2').width('300px');
 		$('.test3').width('300px');
-	}else if(width <= 1100){
+	}else if(width <= 1000){
 		$('.test').addClass('mt-3');
 		$('.test2').width('300px');
 		$('.test3').width('300px');
